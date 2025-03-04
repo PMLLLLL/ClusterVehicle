3e578a47566f6489388d5eb9d3bb62ac87fecac4
PMLLLLL <pengmaolin@proton.me>
2025-03-04 16:51:52 +0800

    将数据归一化操作写进对象中
    将数据归一化操作写进对象中

--- matlab_code/RunCEDAS.m
+++ matlab_code/RunCEDAS.m
@@ -10,7 +10,7 @@ decay = 0.000001; % 衰减因子
 weights = [0.4, 0.2, 0.2, 0.2]; % 时间戳、位置、磁场值、车道号
 initDirection = [1 0 1];
 
-
+path = '../2025.3.3 85个数据汇总_标签.xlsx';
 
 % 数据加载与归一化
 %DataIn = readmatrix('../2025.2.25 大车数据合并_真实标签.xlsx'); % 2025.2.27 小车数据合并_真实标签.xlsx
@@ -18,9 +18,12 @@ initDirection = [1 0 1];
 %DataIn = readmatrix('../C26550、C26580真stream加磁场 .csv');
 %DataIn = readmatrix('../2025.2.27 大车小车数据合并_真实标签.xlsx');
 %DataIn = readmatrix('../2025.3.3 85个数据汇总_标签.xlsx'); % 2025.3.4 11-20个数据汇总_标签.xlsx
-DataIn = readmatrix('../2025.3.4 11-20个数据汇总_标签.xlsx');
-DataIn = sortrows(DataIn, 3); % 按照第三列（位置Y）从小到大排序
-NormalizedData = NormalizeData(DataIn);
+
+% DataIn = readmatrix('../2025.3.4 11-20个数据汇总_标签.xlsx');
+% DataIn = sortrows(DataIn, 3); % 按照第三列（位置Y）从小到大排序
+% NormalizedData = NormalizeData(DataIn);
+
+DataOper = DataProcessing(path);
 
 
 % 产生遍历权重
@@ -33,23 +36,23 @@ for WeightNum = 1:4
     
     % 聚类处理
     for i = 1:count
-        CE = CEDAS(rad, decay,[(1-0.98)/3, 0.98, (1-0.98)/3, (1-0.98)/3], initDirection); % 用于存储聚类结果
+        CE = CEDAS(rad, decay,weights(i,:), initDirection); % 用于存储聚类结果
 
-        for t = 1:size(NormalizedData, 1)
+        for t = 1:size(DataOper.normalizedData, 1)
             % 调用 CEDAS_demo3 算法
-            CE = CE.Clustering(NormalizedData(t,:));
+            CE = CE.Clustering(DataOper.normalizedData(t,:));
         end
     
         % 可视化
         show = Visualize(CE.clusters);
-        show.ContingencyHeatMap1();
-        show.ContingencyHeatMap2();
-        show.ClustersImg;
+        % show.ContingencyHeatMap1();
+        % show.ContingencyHeatMap2();
+        % show.ClustersImg;
         % 计算分类准确度RI
         RI(i) = RandIndex(show.trueLabels,show.clusterLabels);
     
         fprintf('第%d次聚类结果的数量: %d 权重Weights = [ %s] RI = %s \n',...
-                i,length(CE.clusters),sprintf('%.2f ', [(1-0.98)/3, 0.98, (1-0.98)/3, (1-0.98)/3]),sprintf('%.4f ', RI(i)));
+                i,length(CE.clusters),sprintf('%.2f ', weights(i,:)),sprintf('%.4f ', RI(i)));
     end
     
     % 画出变化趋势  
